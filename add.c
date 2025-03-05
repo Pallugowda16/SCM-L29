9c6e888e2d07ea3373128cbe85f765f9a095a544
Pallugowda16 <pallavi.s2@s.amity.edu>
2025-03-05 14:20:01 +0530

    return

--- add.c
+++ add.c
@@ -2,6 +2,6 @@
 void main()
 	int a=3 ,b=4;
 	printf("the sum is:%d",sum);
-	printf("ths diff is:%d",diff);
-	printf("ths prod is:%d",prod);
+	printf("the diff is:%d",diff);
+	printf("the prod is:%d",prod);
 	printf("the div is:%d",div);

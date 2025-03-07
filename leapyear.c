869b9f8a9cf71374dd7a3cb8416159890ded1b1c
Pallugowda16 <pallavi.s2@s.amity.edu>
2025-03-07 21:29:31 +0530

    full pg done

--- leapyear.c
+++ leapyear.c
@@ -3,3 +3,12 @@ int main() {
    int year;
    printf("Enter a year: ");
    scanf("%d", &year);
+   else if (year % 4 == 0) {
+      printf("%d is a leap year.", year);
+   }
+   else {
+      printf("%d is not a leap year.", year);
+   }
+
+   return 0;
+}
